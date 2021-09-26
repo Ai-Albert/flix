1da62940df28a0084607b1fd65c9079da66f324f
Albert Ai <gsbalbert2002@gmail.com>
2021-09-25 23:47:29 -0700

    Added superhero tab

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Albert Ai on 9/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
